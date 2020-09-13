13b86e28dda99a2e736958ce96d732cde71ed132
TejasMalik <tejas.malik97@gmail.com>
2020-09-13 16:25:20 +0530

    MVVM

---
+++ app/src/main/java/com/example/mvvm/ui/viewmodel/GithubViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.mvvm.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.mvvm.data.models.User
+import com.example.mvvm.data.repos.GithubRepository
+import kotlinx.coroutines.*
+
+class GithubViewModel : ViewModel() {
+
+    val users = MutableLiveData<List<User>>()
+    fun fetchUsers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = withContext(Dispatchers.IO) { GithubRepository.getUsers() }
+            if (response.isSuccessful) {
+                response.body().let {
+                    users.postValue(it)
+
+                }
+            }
+        }
+    }
+
+    fun searchUsers(name: String) = liveData(Dispatchers.IO) {
+            val response = withContext(Dispatchers.IO) { GithubRepository.searchUser(name) }
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    emit(it.items)
+                }
+            }
+    }
+}
+
+//fun ViewModel.runIO(dispatchers: CoroutineDispatcher = Dispatchers.IO,
+//                    function: suspend CoroutineStart.() -> Unit) {
+//    viewModelScope.launch(dispatchers) {
+//        function()
+//    }
+//}
